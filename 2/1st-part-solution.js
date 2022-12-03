828816b7112c96f7b604fb3514f4e07c7834030d
Derine Banis <d.banis.13@gmail.com>
2022-12-02 16:39:47 -0800

    Finish day 2 part 1 of advent of code 2022

--- 2/1st-part-solution.js
+++ 2/1st-part-solution.js
@@ -1,29 +1,75 @@
-const fs = require('fs')
-const input = './input.txt'
+const fs = require('fs');
+const input = './input.txt';
 
-const scoreCounter = (input) =>{
-  //Score Values
-  const rock = 1
-  const paper = 2
-  const scissors = 3
+const scoreCounter = (input) => {
+  let total = 0;
 
-  const win = 6
-  const draw = 3
-  const loss = 0
+  const win = 6;
+  const draw = 3;
+  const loss = 0;
 
-  //Symbols Left
-  const a = rock
-  const b = paper
-  const c = scissors
 
-  //Symbols Right
-  const x = rock
-  const y = paper
-  const z = scissors
+  const rock = { opponent: 'a', me: 'x', value: 1 };
+  const paper = { opponent: 'b', me: 'y', value: 2 };
+  const scissors = { opponent: 'c', me: 'z', value: 3 };
 
-  fs.readFile(input, {encoding:'utf-8', flag: 'r'},(err,data)=>{
-    
-  })
-} 
+  //rock beats scis 
+  //paper beats rock
+  //scis beats paper
 
-scoreCounter(input)
+  const data = fs.readFileSync(input, { encoding: 'utf-8', flag: 'r' })
+  data.split('\n').forEach((match) => {
+    match = match.toLocaleLowerCase().split(' ');
+    const opponent = match[0].toLowerCase();
+    const me = match[1].toLowerCase();
+    //Rock - Draw
+    if (opponent === rock.opponent & me === rock.me) {
+      total += rock.value;
+      total += draw;
+    }
+    //Rock - Win
+    if (opponent === scissors.opponent & me === rock.me) {
+      total += rock.value;
+      total += win;
+    }
+    //Rock - Loss
+    if (opponent === rock.opponent & me === scissors.me) {
+      total += scissors.value;
+      total += loss;
+    }
+    //Paper - Draw
+    if (opponent === paper.opponent & me === paper.me) {
+      total += paper.value;
+      total += draw;
+    }
+    //Paper - Win
+    if (opponent === rock.opponent & me === paper.me) {
+      total += paper.value;
+      total += win;
+    }
+    //Paper - Loss
+    if (opponent === paper.opponent & me === rock.me) {
+      total += rock.value;
+      total += loss;
+    }
+    //Scissor - Draw
+    if (opponent === scissors.opponent & me === scissors.me) {
+      total += scissors.value;
+      total += draw;
+    }
+    //Scissor - Win
+    if (opponent === paper.opponent & me === scissors.me) {
+      total += scissors.value;
+      total += win;
+    }
+    //Scissor - Loss
+    if (opponent === scissors.opponent & me === paper.me) {
+      total += paper.value;
+      total += loss;
+    }
+  });
+  
+  return total
+};
+
+console.log(scoreCounter(input));
