24f1bbbf2f5c0b04fc2909d899674870deeb3ca7
Derine Banis <d.banis.13@gmail.com>
2022-12-07 19:23:46 -0800

    Complete part 1 of day 5

--- 5/1st-part-solution.js
+++ 5/1st-part-solution.js
@@ -7,14 +7,76 @@ const supplyStacks = (input) => {
 
   let stackDiagram = data.split('\n\n').shift().split('\n')
   stackDiagram = stackDiagram.map(e=>e.split('\t'))
-  const stackDiagramNumberPositions = stackDiagram.pop().shift().split('')
+  stackDiagram.pop()
+  let stackObj = {}
+  let counter = Math.ceil(stackDiagram[0][0].length / 4 )
+  while(counter > 0){
+    stackObj[counter] = []
+    counter --
+  }
 
-  const instructions = data.split('\n\n').pop().split('\n')
+
+  stackDiagram.reverse().forEach((stack,index) => {
+    let stackNumber = 1
+    for(let i = 1; i < stack[0].length; i+=4){
+      stackObj[stackNumber].push(stack[0][i])
+      stackNumber++
+    }
+
+    stackNumber = 1
+  })
+
+  // console.log(stackObj)
+
+  let instructions = data.split('\n\n').pop().split('\n')
+  instructions = instructions.map(instruction=>{
+    instruction = instruction.slice(instruction.indexOf(' ') + 1)
+    const amountToMove = parseInt(instruction.slice(0, instruction.indexOf(' ')))
+    instruction = instruction.slice(instruction.indexOf(' ') + 1)
+    instruction = instruction.slice(instruction.indexOf(' ') + 1)
+    const stackToTakeFrom = parseInt(instruction.slice(0, instruction.indexOf(' ')))
+    instruction = instruction.slice(instruction.indexOf(' ') + 1)
+    instruction = instruction.slice(instruction.indexOf(' ') + 1)
+    const stackToMoveTo = parseInt(instruction[0])
+    return   {
+      amountToMove,
+      stackToTakeFrom,
+      stackToMoveTo
+    }
+  })
+
+  // console.log(instructions)
+
+  
+
+  instructions.forEach(instruction=>{
+    let counter = 0;
+    while(counter < instruction.amountToMove){
+      while(stackObj[instruction.stackToTakeFrom][stackObj[instruction.stackToTakeFrom].length -1] === ' '){
+        stackObj[instruction.stackToTakeFrom].pop()
+
+      }
+  
+      stackObj[instruction.stackToMoveTo].push(stackObj[instruction.stackToTakeFrom].pop())
+      counter++
+    }
+  })
+
+  return Object.values(stackObj).map(val=>{
+    while(val[val.length-1] === ' '){
+      val.pop()
+    }
+
+    return val.slice(-1)
+
+  }).join('')
 
 
-  console.log(stackDiagram)
-  return stackDiagramNumberPositions
+
+
+
+  return null
 }
 console.log('Test Case Stacks')
-console.log(supplyStacks(testData))
+console.log(supplyStacks(realData))
 console.log('\n')
