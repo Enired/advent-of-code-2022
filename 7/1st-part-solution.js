5cf78954d04cd060c58b719e4061246a47bc60ef
Derine Banis <d.banis.13@gmail.com>
2022-12-09 17:13:04 -0800

    Possible solution. Not optimal due to nested for loop. Will test if functions correctly first then optimize

--- 7/1st-part-solution.js
+++ 7/1st-part-solution.js
@@ -3,24 +3,28 @@ const testInput = './test-input.txt'
 
 const measureD = (input) => {
   const terminalOutput = fs.readFileSync(testInput, 'utf-8').split('\n');
-  const maxSize = 10000;
+  const maxSize = 100000;
   
   const sysDirectories = {
-    '/': []
+    '/': {
+      subfolders: [],
+      size: 0
+    }
   }
-  
   
   //Create Directories
   terminalOutput.forEach(command => {
     if(command.includes('dir')){
       const directory = command.split(' ')[1];
-      sysDirectories[directory] = [];
+      sysDirectories[directory] = { subfolders: [],
+        size: 0};
     }
   })
   let dirHistory = []
   let currentDir
   
 
+  //Determining Size of each directory not including the size of any subfolders
   terminalOutput.forEach(command=>{
     if(command.includes('cd')){
       //Move out
@@ -33,23 +37,38 @@ const measureD = (input) => {
       }
 
       currentDir = dirHistory.slice(-1)[0];
-      console.log(currentDir)
 
     }
     else{
-  
       if(!command.includes('$')){
-        sysDirectories[currentDir].push(command)
-
+        if(command.includes('dir')){
+          const dirLetter = command.split(' ')[1]
+          sysDirectories[currentDir].subfolders.push(dirLetter)
+        }
+        else{
+          const fileSize = parseInt(command.split(' ')[0])
+          sysDirectories[currentDir].size += fileSize
+          
+        }
       }
     }
-
-
-
-    // console.log(dirHistory)
   })
 
-  return sysDirectories
+
+  Object.values(sysDirectories).forEach((dir)=>{
+    dir.subfolders.forEach((subDir => {
+      dir.size += sysDirectories[subDir].size
+    }))
+  })
+
+  let total = 0;
+  Object.values(sysDirectories).forEach(dir=>{
+    if(dir.size <= maxSize){
+      total+=dir.size
+    }
+  })
+
+  return total
 }
 
 console.log(measureD(testInput)) // TestData
