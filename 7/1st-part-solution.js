cf8d713f6466b03eb2f14bcc805ce4b863a68f5b
Derine Banis <d.banis.13@gmail.com>
2022-12-09 16:32:46 -0800

    Break commands down into directories and files

--- 7/1st-part-solution.js
+++ 7/1st-part-solution.js
@@ -2,7 +2,54 @@ const fs = require('fs')
 const testInput = './test-input.txt'
 
 const measureD = (input) => {
-  const terminalOutput = fs.readFileSync(testInput, 'utf-8');
+  const terminalOutput = fs.readFileSync(testInput, 'utf-8').split('\n');
+  const maxSize = 10000;
+  
+  const sysDirectories = {
+    '/': []
+  }
+  
+  
+  //Create Directories
+  terminalOutput.forEach(command => {
+    if(command.includes('dir')){
+      const directory = command.split(' ')[1];
+      sysDirectories[directory] = [];
+    }
+  })
+  let dirHistory = []
+  let currentDir
+  
 
-  return terminalOutput
+  terminalOutput.forEach(command=>{
+    if(command.includes('cd')){
+      //Move out
+      if(command.includes('..')){
+        dirHistory.pop()
+      }
+      else{
+        const cdCommand = command.split(' ')
+        dirHistory.push(cdCommand[2])
+      }
+
+      currentDir = dirHistory.slice(-1)[0];
+      console.log(currentDir)
+
+    }
+    else{
+  
+      if(!command.includes('$')){
+        sysDirectories[currentDir].push(command)
+
+      }
+    }
+
+
+
+    // console.log(dirHistory)
+  })
+
+  return sysDirectories
 }
+
+console.log(measureD(testInput)) // TestData
